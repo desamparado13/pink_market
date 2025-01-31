b2c987d525f22d569807cf69f9eb0abd9227544b
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2025-01-31 19:29:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<link rel="icon" href="images/pink_market_icon.jpg">
+<link rel="stylesheet" href="style.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pink Market</title>
+</head>
+<body>
+    <nav>
+        <ul class="sidebar">
+            <li><a href="">Pink Market</a></li>
+            <li><a href="">Sell</a></li>
+            <li><a href="">Track Order</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Signup</a></li>
+        </ul>
+        <ul>
+            <li><a href="">Pink Market</a></li>
+            <li><a href="">Sell</a></li>
+            <li><a href="">Track Order</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Signup</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
