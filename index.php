96253d0d55bc917722215ff1a172f33182e9bbf1
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2025-02-03 18:27:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,10 +27,42 @@
     </nav>
     <div class="main_body">
         <div class="search-wrapper">
-            <form action="">
+            <form class="form-wrapper"action="">
                 <input class="searchbar" type="text" placeholder="Search in Pink Market">
-                <button type="submit">Search</button>
+                <button type="submit">Find</button>
             </form>
+        </div>
+
+    <div class="slideshow-container">
+            <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 4</div>
+                <img class="slide-images" src="images/ads_banner_1.jpg">
+        </div>
+        <div class="mySlides fade">
+        <div class="numbertext">2 / 4</div>
+            <img class="slide-images" src="images/ads_banner_2.jpeg">
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 4</div>
+                <img class="slide-images" src="images/ads_banner_3.jpeg">
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 4</div>
+                <img class="slide-images" src="images/ads_banner_4.jpeg">
+        </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
+
+            <!-- The dots/circles -->
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
         </div>
     </div>
 
@@ -68,6 +100,35 @@
             const sidebar = document.querySelector('.sidebar')
             sidebar.style.display = 'none'
         }
+
+                    let slideIndex = 1;
+            showSlides(slideIndex);
+
+            // Next/previous controls
+            function plusSlides(n) {
+            showSlides(slideIndex += n);
+            }
+
+            // Thumbnail image controls
+            function currentSlide(n) {
+            showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            }
     </script>
 </body>
 </html>
