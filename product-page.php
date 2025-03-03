c0e2a821fa7af1820930af62907b1f8011c6256d
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2025-03-02 23:14:14 -0800

    Create product-page.php

---
+++ product-page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<link rel="icon" href="images/pink_market_icon.jpg">
+<link rel="stylesheet" href="style.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pink Market</title>
+</head>
+<body>
+    <?php
+        include 'nav.php'
+    ?>
+    <div class="product-wrapper">
+        <div class="product-container">
+            <div class="">
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
