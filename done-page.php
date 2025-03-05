04fc814a1163227c8288cc6d6319db6d849b4206
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2025-03-05 15:48:12 +0800

    Create done-page.php

---
+++ done-page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'nav.php'
+    ?>
+    <div class="receipt-container">
+        <h1>Thank you for your Purchase!</h1>
+        <h3>Order No. 82732872</h3>
+        <h3>Chuchuchu</h3>
+    </div>
+</body>
+</html>
