ebdeefe03e3efe02227cbfd1910e129f61ba87b3
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2025-03-05 20:10:15 -0800

    Add files via upload

---
+++ buyer-login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="login-register.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<nav>
+    
+</nav>
+<body>
+    <div class="container">
+        <div class="form-box" id="login-form">
+            <form action="">
+                <h2>Login</h2>
+                <input type="username" name="username" placeholder="Username" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit" name="login">Login</button>
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
